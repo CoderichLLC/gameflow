cdfdf752d6bc2b1e3cc0cd5b12d5ce3be4cab016
Richard Livolsi <richard.livolsi@goziohealth.com>
2025-06-11 10:17:40 -0400

    Cascade renamed to Force

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
-## 1.3.0
-- Added Cascade class
+## 1.3.1
+- Added Force class
 - Stream supports batch
 - abortListeners now an array like the other listeners
 - startListeners called AFTER first step is called
--- src/Force.js
+++ src/Force.js
@@ -1,15 +1,15 @@
 const Action = require('./Action');
 
 /**
- * Continuously repeat the stream->action until the stream is aborted
+ * Continuously force-repeat the stream->action until the stream is aborted
  */
-module.exports = class Cascade {
+module.exports = class Force {
   constructor(...steps) {
     return async (data, context) => {
       let aborted = false;
       const onAbort = () => (aborted = true);
       context.stream.once('abort', onAbort);
-      const action = new Action('cascade', steps.flat());
+      const action = new Action('force', steps.flat());
       await action(data, context);
       if (!aborted) context.actor.stream(context.stream, context.action);
       context.stream.off('abort', onAbort);
